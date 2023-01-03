670cc04e6fc495e0983709842e1b003ef959aa3f
Matt Holmstrom <mjholmstr@gmail.com>
2023-01-03 11:18:43 -0600

    Prompt for user input and validate against choices

--- script.js
+++ script.js
@@ -1,33 +1,60 @@
-let choices = ['Rock', 'Paper', 'Scissors'];
+const choices = ['rock', 'paper', 'scissors'];
+
+function game() {
+    playRound();
+}
 
 function getComputerChoice() {
     let compChoice = choices[Math.floor(Math.random() * choices.length)];
     return compChoice;
 }
 
+function getPlayerChoice() {
+    let input = prompt("Type Rock, Paper, or Scissors");
+    while (input == null) {
+        input = prompt("Type Rock, Paper, or Scissors");
+    }
+    input = input.toLowerCase();
+    let check = validateInput(input);
+    if (check == true) {
+        console.log(input);
+    }
+}
+
+getPlayerChoice();
+
+//Validate user input
+function validateInput(userInput) {
+    if (choices.includes(userInput)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 
 function playRound(playerSelection, computerSelection) {
 
     if (playerSelection.toLowerCase() === 'rock') {
-        if (computerSelection === 'Rock') {
+        if (computerSelection === 'rock') {
             return "Tie!"
-        } else if (computerSelection === 'Paper') {
+        } else if (computerSelection === 'paper') {
             return "You lose! Paper beats Rock"
         } else {
             return "You Win! Rock beats Scissors"
         }
     } else if (playerSelection.toLowerCase() === 'paper') {
-        if (computerSelection === 'Rock') {
+        if (computerSelection === 'rock') {
             return "You Win! Paper beats Rock"
-        } else if (computerSelection === 'Paper') {
+        } else if (computerSelection === 'paper') {
             return "Tie!"
         } else {
             return "You Lose! Scissors beat Paper"
         }
     } else if (playerSelection.toLowerCase() === 'scissors') {
-        if (computerSelection === 'Rock') {
+        if (computerSelection === 'rock') {
             return "You Lose! Rock beats Scissors"
-        } else if (computerSelection === 'Paper') {
+        } else if (computerSelection === 'paper') {
             return "You Win! Scissors beat Paper"
         } else {
             return "Tie"
